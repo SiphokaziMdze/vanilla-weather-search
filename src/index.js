56c7ae0bbe1eab4ad7ef521020526cdaadffa6bb
SIPHOKAZI VERONICA MDZE <veronicamdze@gmail.com>
2023-11-05 21:51:02 +0000

    Updated index.js and a weather icon

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ function updateWeather(response){
     let windElement = document.querySelector("#wind");
     let timeElement = document.querySelector("#time")
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#weather-app-icon");
+    
 
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon"/>`;
     timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML = Math.round(temperature);
     //console.log(response.data);
